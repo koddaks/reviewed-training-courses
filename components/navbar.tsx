c973a63ad824d862c244d08144fe008bf37d7775
koddaks <koss199710@gmail.com>
2024-07-31 17:11:31 +0300

    Navbar Component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 const Navbar = () => {
     return (
         <div className="flex items-center justify-between gap-8">
-           <div className="flex  w-full">
+           <div className="flex w-full">
               <Button size="sm" variant="ghost" asChild>
                 <Link href="/courses">Курси</Link>
               </Button>
