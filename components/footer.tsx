f0c98393c653467bc05d1fb6bc3b0b87f8849aaa
koddaks <koss199710@gmail.com>
2024-07-31 17:10:35 +0300

    footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,22 @@
-import { Button } from '@/components/ui/button';
 import { Logo } from './logo';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <div className="flex flex-row fixed bottom-0 w-full h-[300px] p-4 border-t bg-black-1000 text-white">
-      <div className="md:max-w-screen-2xl lg:max-w-[1280px] mx-auto flex items-center w-full justify-between">
+      <div className="md:max-w-screen-2xl lg:max-w-[1280px] mx-auto flex items-center w-full">
         <Logo variant="white" />
-        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button size="sm" variant="ghost">
-            Pivacy Policy
-          </Button>
-          <Button size="sm" variant="ghost">
-            Terms of Service
-          </Button>
+        <div className="relative left-1/3 transform -translate-x-1/3 flex flex-row gap-24 justify-self-end">
+          <nav className="md:flex md:w-auto flex-col w-full gap-1 justify-start">
+            <p>Навігація</p>
+            <Link href="/about-us">Про нас</Link>
+            <Link href="/courses">Курси</Link>
+          </nav>
+          <nav className="md:flex md:w-auto flex-col w-full gap-1 justify-start">
+            <p>Навігація</p>
+            <Link href="/about-us">Про нас</Link>
+            <Link href="/courses">Курси</Link>
+          </nav>
         </div>
       </div>
     </div>
