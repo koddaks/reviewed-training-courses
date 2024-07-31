98b47f18b804c5cb03e353203074a6d76295744b
koddaks <koss199710@gmail.com>
2024-07-31 17:11:52 +0300

    add daylight button variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
-        daylight: 'bg-daylight text-bluelight hover:bg-daylight/90',
+        daylight: 'bg-daylight text-bluelight hover:bg-daylight/90',        
       },
       size: {
         default: 'h-10 px-4 py-2',
