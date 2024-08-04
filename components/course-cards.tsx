4ee904f9beea0a9f2e582cd5b281d74815a785c1
koddaks <koss199710@gmail.com>
2024-08-04 23:23:19 +0300

    make test carousel autoplay

--- components/course-cards.tsx
+++ components/course-cards.tsx
@@ -1,17 +1,32 @@
 'use client';
 
+import { useRef } from 'react';
 import { courses } from '@/mock/courses';
 import CourseItem from './course-item';
-import { Carousel, CarouselContent, CarouselItem, CarouselDots } from './ui/carousel';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselDots,
+} from './ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
 
 const CourseCards = () => {
+  const plugin = useRef(Autoplay({ delay: 3000, stopOnInteraction: true, }));
+
   return (
     <section className="py-12">
       <div className="container mx-auto">
         <h2 className="text-4xl font-bold mb-6">Найкращі школи</h2>
 
         <div className="w-full flex justify-center">
-          <Carousel className="w-full max-w-xs sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-7xl">
+          <Carousel
+            className="w-full max-w-xs sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-7xl"
+            plugins={[plugin.current]}             
+            onMouseEnter={plugin.current.stop}
+            onMouseLeave={plugin.current.reset}
+            
+          >
             <CarouselContent className=" flex">
               {Array.from(courses).map((course, index) => (
                 <CarouselItem key={index} className="basis-1/4">
@@ -26,8 +41,7 @@ const CourseCards = () => {
               ))}
             </CarouselContent>
             <div className="flex justify-center">
-             <CarouselDots/>
-
+              <CarouselDots />
             </div>
           </Carousel>
         </div>
--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -3,6 +3,8 @@
 import * as React from "react";
 import useEmblaCarousel, { type UseEmblaCarouselType } from "embla-carousel-react";
 import { cn } from "@/lib/utils";
+
+
 type CarouselApi = UseEmblaCarouselType[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
 type CarouselOptions = UseCarouselParameters[0];
