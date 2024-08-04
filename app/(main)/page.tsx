c45454b9fa69e4acdf4dcb398dfee940dd866349
koddaks <koss199710@gmail.com>
2024-08-04 16:38:37 +0300

    delete test buttons

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,18 +1,10 @@
-import { ApproveButton } from '@/components/buttons/approve-button';
-import { FavoriteButton } from '@/components/buttons/favorite-button';
-import { TagButton } from '@/components/buttons/tag-button';
-import { RejectButton } from '@/components/buttons/reject-button';
+import SearchSection from '@/components/search-section';
 
 const MainPage = () => {
   return (
-    <div className="">
-      <div className="flex flex-col gap-2 w-[148px]">
-        <ApproveButton />
-        <RejectButton />
-        <FavoriteButton />
-        <TagButton/>
-      </div>
-    </div>
+    <>
+      <SearchSection />
+    </>
   );
 };
 
